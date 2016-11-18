18ead405ab01fafa907cc41cdeb91def5bf3bc7b
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-11-18 10:49:44 -0800

    add ServerDetails activity

---
+++ app/src/main/java/com/epicodus/headlamp/ServerDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.epicodus.headlamp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ServerDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_server_details);
+    }
+}
