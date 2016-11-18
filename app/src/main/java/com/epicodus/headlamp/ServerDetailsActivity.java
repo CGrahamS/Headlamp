a6e80ae05b0d4bc9701fba0e6ced1764f0cfe95f
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-11-18 14:11:20 -0800

    add serverDetailValue and serverDetailLabel arrays to string.xml, add ListView to ServerDetailsActivity, add ArrayAdapted to ServerDetailsActivity

--- app/src/main/java/com/epicodus/headlamp/ServerDetailsActivity.java
+++ app/src/main/java/com/epicodus/headlamp/ServerDetailsActivity.java
@@ -4,7 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -12,7 +16,12 @@ import butterknife.ButterKnife;
 public class ServerDetailsActivity extends AppCompatActivity {
     public static final String TAG = ServerDetailsActivity.class.getSimpleName();
     private String mServerDomain;
+    private String[] mServerDetailLabels;
+    private String[] mServerDetailValues; //Simulated values. These will eventually be retrieved via an API Get request
+    private String mFormattedDetail = "formattedDetail";
+    private List<String> mFormattedDetailArray;
     @Bind(R.id.serverDomainTextView) TextView mServerDomainTextView;
+    @Bind(R.id.serverDetailListView) ListView mServerDetailListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +36,16 @@ public class ServerDetailsActivity extends AppCompatActivity {
         Log.i(TAG, mServerDomain);
         //set serverHeader textView text to new string variable
         mServerDomainTextView.setText(mServerDomain);
+        //Populate serverDetails ArrayList with simulated values
+        mServerDetailValues = getResources().getStringArray(R.array.server_detail_values);
+        mServerDetailLabels = getResources().getStringArray(R.array.server_detail_labels);
+        mFormattedDetail = String.format(mServerDetailLabels[0], mServerDetailValues[0]);
+//        mFormattedDetailArray.add(mFormattedDetail);
+        //create ArrayAdapter
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+        mServerDetailListView.setAdapter(adapter);
+// FIXME  for(int i = 0; i < mServerDetailLabels.length; i++) {
+//            mFormattedDetail+i = String.format(mServerDetailLabels[i], mServerDetailValues[i]);
+//        }
     }
 }
